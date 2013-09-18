db8ef5a39ede9c85a825663486f997a986033221
Lynn Conway <lconner@bendcable.com>
2013-09-18 15:29:23 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HealthJournal::Application.config.secret_token = '8c146c9ba9366085e1c8ab59c95224d6a56d94abe0a57143b463a46474dbb6bf34d196fef520db7dae389bbf1efe7056f53674c5e9ede423ecd664ad662717b5'
